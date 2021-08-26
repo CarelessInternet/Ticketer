8b505291afb7d6ac16b1345cd4a4ded2eafce932
CarelessInternet <myinternetisg00d@gmail.com>
2021-08-26 21:57:34 +0200

    added on guid join event

---
+++ events/guild/guildCreate.js
@@ -0,0 +1,19 @@
+const {readFileSync} = require('fs');
+
+function guildCreate(client, Discord, prefix, guild) {
+  if (!guild.systemChannel || !guild.me.permissions.has('ADMINISTRATOR')) return;
+
+  const inviteField = readFileSync('./info/invitelink.txt', 'utf8');
+  const supportField = readFileSync('./info/support.txt', 'utf8');
+  const embed = new Discord.MessageEmbed()
+  .setColor('RANDOM')
+  .setTitle('Hello!')
+  .setDescription(`Thank you for inviting me!\nTo view all of the commands, please use \`${prefix}help\``)
+  .addField('Support Server', supportField)
+  .addField('Invite Link', inviteField)
+  .setTimestamp();
+
+  guild.systemChannel.send({embeds: [embed]}).catch(console.error);
+}
+
+module.exports = guildCreate;

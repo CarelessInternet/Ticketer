4065e2b1a2a18f0971acb5371366d14bdaaaa304
CarelessInternet <myinternetisg00d@gmail.com>
2021-08-26 21:56:21 +0200

    added info files

---
+++ info/data-options.js
@@ -0,0 +1,13 @@
+const map = new Map();
+map.set(1, 'Sub-Command');
+map.set(2, 'Sub-Command-Group');
+map.set(3, 'Text');
+map.set(4, 'Integer');
+map.set(5, 'True/False');
+map.set(6, 'User');
+map.set(7, 'Channel');
+map.set(8, 'Role');
+map.set(9, 'Mentionable');
+map.set(10, 'Number');
+
+module.exports = map;

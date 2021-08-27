088b495e37cac23dc1246cd577fc88c6501ff49f
CarelessInternet <myinternetisg00d@gmail.com>
2021-08-27 19:45:32 +0200

    added database files

---
+++ mysql.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+
+const connection = require('./db');
+const createGuildMemberEvent =
+`
+CREATE TABLE IF NOT EXISTS GuildMemberEvent(
+  ID int NOT NULL AUTO_INCREMENT,
+  GuildID bigint(20) NOT NULL UNIQUE,
+  ChannelID bigint(20) NOT NULL,
+  PRIMARY KEY (ID)
+)
+`;
+
+(async () => {
+  try {
+    await connection.execute(createGuildMemberEvent);
+    console.log('Successfully created all required tables!');
+  } catch(err) {
+    console.error(err);
+  }
+})();

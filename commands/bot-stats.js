c728968b344b179e10138d169e2deb16aca5d538
CarelessInternet <myinternetisg00d@gmail.com>
2021-08-27 17:14:28 +0200

    added bot-stats command

---
+++ commands/bot-stats.js
@@ -0,0 +1,45 @@
+const {MessageEmbed} = require('discord.js');
+
+module.exports = {
+  name: 'bot-stats',
+  data: {
+    name: "bot-stats",
+    description: "Returns info about the bot's stats",
+    category: "utility",
+    options: [],
+    examples: [
+      "bot-stats"
+    ]
+  },
+  async execute(interaction) {
+    try {
+      const {shard} = interaction.client;
+      const promises = [
+        shard.fetchClientValues('guilds.cache.size'),
+        shard.fetchClientValues('emojis.cache.size'),
+        shard.fetchClientValues('channels.cache.size'),
+        shard.fetchClientValues('users.cache.size'),
+        shard.broadcastEval(c => c.guilds.cache.reduce((acc, curr) => acc + curr.memberCount, 0))
+      ];
+
+      const [guildSize, emojiSize, channelSize, userSize, memberSize] = await Promise.all(promises);
+      const embed = new MessageEmbed()
+      .setColor('RANDOM')
+      .setAuthor(interaction.user.tag, interaction.user.avatarURL())
+      .setTitle('Bot Stats')
+      .setDescription('Shows info about the bot\'s stats')
+      .addField('Servers', '📊 ' + guildSize.reduce((acc, curr) => acc + curr, 0), true)
+      .addField('Server Members', '👥 ' + memberSize.reduce((acc, curr) => acc + curr, 0), true)
+      .addField('Cached Users', '👤 ' + userSize.reduce((acc, curr) => acc + curr, 0), true)
+      .addField('\u200B', '\u200B')
+      .addField('Channels', '📺 ' + channelSize.reduce((acc, curr) => acc + curr, 0), true)
+      .addField('Emojis', '💩 ' + emojiSize.reduce((acc, curr) => acc + curr, 0), true)
+      .setTimestamp();
+
+      interaction.reply({embeds: [embed]});
+    } catch(err) {
+      console.error(err);
+      interaction.reply({content: 'An unknown error occured whilst fetching data, please try again later', ephemeral: true});
+    }
+  }
+}

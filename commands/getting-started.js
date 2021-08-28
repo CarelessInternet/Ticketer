fafbf9d4440579efeeb69266ad31c2efc2aaacf2
CarelessInternet <myinternetisg00d@gmail.com>
2021-08-28 18:12:48 +0200

    added getting started file and command

---
+++ commands/getting-started.js
@@ -0,0 +1,35 @@
+const {version} = require('../package.json');
+const {readFileSync} = require('fs');
+const {MessageEmbed} = require('discord.js');
+
+module.exports = {
+  name: 'getting-started',
+  data: {
+    name: "getting-started",
+    description: "Shows how to get started with using the bot",
+    category: "utility",
+    options: [],
+    examples: [
+      "getting-started"
+    ]
+  },
+  execute(interaction) {
+    try {
+      const startingFile = readFileSync('./info/gettingstarted.txt', 'utf8');
+      const inviteFile = readFileSync('./info/invitelink.txt', 'utf8');
+      const embed = new MessageEmbed()
+      .setColor('RANDOM')
+      .setAuthor(interaction.user.tag, interaction.user.avatarURL())
+      .setTitle('Getting Started')
+      .setDescription(inviteFile)
+      .addField('Steps', startingFile)
+      .setTimestamp()
+      .setFooter(`Version ${version}`);
+
+      interaction.reply({embeds: [embed]});
+    } catch(err) {
+      console.error(err);
+      interaction.reply({content: 'An unknown error occured, please try again later', ephemeral: true});
+    }
+  }
+}
--- events/guild/guildCreate.js
+++ events/guild/guildCreate.js
@@ -5,12 +5,14 @@ function guildCreate(client, Discord, prefix, guild) {
 
   const inviteField = readFileSync('./info/invitelink.txt', 'utf8');
   const supportField = readFileSync('./info/support.txt', 'utf8');
+  const startingField = readFileSync('./info/gettingstarted.txt', 'utf8');
   const embed = new Discord.MessageEmbed()
   .setColor('RANDOM')
   .setTitle('Hello!')
   .setDescription(`Thank you for inviting me!\nTo view all of the commands, please use \`${prefix}help\``)
   .addField('Support Server', supportField)
   .addField('Invite Link', inviteField)
+  .addField('Getting Started', startingField)
   .setTimestamp();
 
   guild.systemChannel.send({embeds: [embed]}).catch(console.error);

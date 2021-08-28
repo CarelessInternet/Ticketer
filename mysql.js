d743d28c310bb60ed3e994001f4185810829ce6a
CarelessInternet <myinternetisg00d@gmail.com>
2021-08-28 02:15:23 +0200

    added table

--- mysql.js
+++ mysql.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const connection = require('./db');
 const createGuildMemberEvent =
 `
-CREATE TABLE IF NOT EXISTS GuildMemberEvent(
+CREATE TABLE IF NOT EXISTS GuildMemberEvent (
   ID int NOT NULL AUTO_INCREMENT,
   GuildID bigint(20) NOT NULL UNIQUE,
   ChannelID bigint(20) NOT NULL,
@@ -11,10 +11,20 @@ CREATE TABLE IF NOT EXISTS GuildMemberEvent(
   PRIMARY KEY (ID)
 )
 `;
+const createTicketingManagers =
+`
+CREATE TABLE IF NOT EXISTS TicketingManagers (
+  ID int NOT NULL AUTO_INCREMENT,
+  GuildID bigint(20) NOT NULL UNIQUE,
+  RoleID bigint(20) NOT NULL,
+  PRIMARY KEY (ID)
+)
+`;
 
 (async () => {
   try {
     await connection.execute(createGuildMemberEvent);
+    await connection.execute(createTicketingManagers);
     console.log('Successfully created all required tables!');
   } catch(err) {
     console.error(err);

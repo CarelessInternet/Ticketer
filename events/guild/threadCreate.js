c447709d64bebab7598819f055b97512bdc4f0ee
CarelessInternet <myinternetisg00d@gmail.com>
2021-08-29 15:42:34 +0200

    added checking for id

--- events/guild/threadCreate.js
+++ events/guild/threadCreate.js
@@ -2,7 +2,8 @@ function onCreate(client, Discord, prefix, thread) {
   // delete the system message that shows "bla bla bla started a thread"
   try {
     const {guild, parent} = thread;
-    if (guild.me.permissions.has('MANAGE_MESSAGES') && parent.lastMessage.system && parent.name === 'support') parent.lastMessage.delete();
+    const {lastMessage, name} = parent;
+    if (guild.me.permissions.has('MANAGE_MESSAGES') && lastMessage.system && lastMessage.author.id === client.user.id && name === 'support') lastMessage.delete();
   } catch(err) {
     console.error(err);
   }

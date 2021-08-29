e6dc91d5baef024bbc45c10b2fb4c1ea4def8aa7
CarelessInternet <myinternetisg00d@gmail.com>
2021-08-29 14:11:48 +0200

    changed from command.name to command.data.name

--- handlers/command_handler.js
+++ handlers/command_handler.js
@@ -4,7 +4,7 @@ function handler(client, Discord) {
   const files = readdirSync('./commands/').filter(file => file.endsWith('.js'));
   files.forEach(file => {
     const command = require(`../commands/${file}`);
-    if (command.name) client.commands.set(command.name, command);
+    client.commands.set(command.data.name, command);
   });
 }
 

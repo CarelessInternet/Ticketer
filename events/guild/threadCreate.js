951c8b1b886575cda7e5f5a92d49e253d98e0baf
CarelessInternet <myinternetisg00d@gmail.com>
2021-08-29 14:12:25 +0200

    if parent of thread is support, delete system message

--- events/guild/threadCreate.js
+++ events/guild/threadCreate.js
@@ -1,6 +1,11 @@
 function onCreate(client, Discord, prefix, thread) {
   // delete the system message that shows "bla bla bla started a thread"
-  thread.parent.lastMessage.delete();
+  try {
+    const {guild, parent} = thread;
+    if (guild.me.permissions.has('MANAGE_MESSAGES') && parent.lastMessage.system && parent.name === 'support') parent.lastMessage.delete();
+  } catch(err) {
+    console.error(err);
+  }
 }
 
 module.exports = onCreate;

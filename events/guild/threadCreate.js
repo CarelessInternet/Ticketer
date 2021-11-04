f12527d187e0c65b485e42463130400bd9fd096c
CarelessInternet <myinternetisg00d@gmail.com>
2021-11-04 14:37:06 +0100

    fixed bug where the bot wont delete 'ticketer created a thread'

--- events/guild/threadCreate.js
+++ events/guild/threadCreate.js
@@ -1,9 +1,33 @@
-function onCreate(client, Discord, prefix, thread) {
+const connection = require('../../db');
+
+function ifExists(guildId) {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const [rows] = await connection.execute('SELECT * FROM TicketingManagers WHERE GuildID = ?', [guildId]);
+      resolve(rows[0] ?? false);
+    } catch(err) {
+      reject(err);
+    }
+  });
+}
+
+async function onCreate(client, Discord, prefix, thread) {
   // delete the system message that shows "bla bla bla started a thread"
   try {
-    const {guild, parent} = thread;
+    const {guild, guildId, parent} = thread;
     const {lastMessage, name} = parent;
-    if (guild.me.permissions.has('MANAGE_MESSAGES') && lastMessage.system && lastMessage.author.id === client.user.id && name.toLowerCase() === 'support') lastMessage.delete();
+
+    const record = await ifExists(guildId);
+    
+    if (thread.permissionsFor(guild.me).has('MANAGE_MESSAGES') && lastMessage.system && lastMessage.author.id === client.user.id) {
+      if (record && record['SupportChannel'] === parent.id) {
+        lastMessage.delete();
+      } else if (name.toLowerCase() === 'support') {
+        lastMessage.delete();
+      } else {
+        return;
+      }
+    }
   } catch(err) {
     console.error(err);
   }

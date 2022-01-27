eb8fba52d86c63dd8b0803dbada3dff3173a3aaf
CarelessInternet <myinternetisg00d@gmail.com>
2022-01-27 22:45:23 +0100

    feat(command): add execute for button interactions

--- src/events/server/interactionCreate.ts
+++ src/events/server/interactionCreate.ts
@@ -1,33 +1,46 @@
-import { Constants, type Interaction } from 'discord.js';
+import {
+	Constants,
+	type MessageInteraction,
+	type Interaction
+} from 'discord.js';
 import type { Event } from '../../types';
 
 const event: Event = {
 	name: Constants.Events.INTERACTION_CREATE,
 	execute: (client, interaction: Interaction) => {
-		if (!interaction.isCommand()) return;
-		if (!interaction.inGuild()) {
-			return interaction.reply({
-				content: 'You need to be in a server to use my commands',
-				ephemeral: true
-			});
+		if (interaction.isCommand() || interaction.isButton()) {
+			if (!interaction.inGuild()) {
+				return interaction.reply({
+					content: 'You need to be in a server to use my commands',
+					ephemeral: true
+				});
+			}
+
+			const cmd = interaction.isCommand()
+				? interaction.commandName
+				: (interaction.message.interaction as MessageInteraction).commandName;
+			const command = client.commands.get(cmd);
+
+			if (!command) return;
+
+			if (
+				(command.ownerOnly || command.privateGuildAndOwnerOnly) &&
+				interaction.user.id !== process.env.DISCORD_OWNER_ID
+			) {
+				return interaction.reply({
+					content: 'You need to be the owner of the bot to run this command',
+					ephemeral: true
+				});
+			}
+
+			if (interaction.isCommand()) {
+				command.execute({ client, interaction });
+			} else {
+				command.buttonExecute?.({ client, interaction });
+			}
+		} else {
+			return;
 		}
-
-		const cmd = interaction.commandName;
-		const command = client.commands.get(cmd);
-
-		if (!command) return;
-
-		if (
-			(command.ownerOnly || command.privateGuildAndOwnerOnly) &&
-			interaction.user.id !== process.env.DISCORD_OWNER_ID
-		) {
-			return interaction.reply({
-				content: 'You need to be the owner of the bot to run this command',
-				ephemeral: true
-			});
-		}
-
-		command.execute({ client, interaction });
 	}
 };
 

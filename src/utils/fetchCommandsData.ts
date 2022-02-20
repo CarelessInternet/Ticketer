48981157dd9da91f24f1b70939fd63a6c0c20f95
CarelessInternet <59174259+CarelessInternet@users.noreply.github.com>
2022-02-20 20:34:50 +0100

    Apply suggestions from code review

--- src/utils/fetchCommandsData.ts
+++ src/utils/fetchCommandsData.ts
@@ -1,20 +1,18 @@
-import fg from 'fast-glob';
 import type { RESTPostAPIApplicationCommandsJSONBody } from 'discord-api-types/v9';
-import { resolve } from 'path';
+import { fetchCommands } from '.';
 import type { Command } from '../types';
 
 /**
  * Fetches the slash command builder data for each command and returns global commands array and owner commands array in an array
  */
 export const fetchCommandsData = async () => {
-	const files = await fg(resolve(__dirname, '../commands/**/*.js'));
+	const commands = await fetchCommands();
 
 	// add each command data into an array, owner only commands go into a seperate array
 	const ownerGuildCommands: RESTPostAPIApplicationCommandsJSONBody[] = [];
 	const globalCommands: RESTPostAPIApplicationCommandsJSONBody[] = [];
 
-	for await (const file of files) {
-		const { default: command }: { default: Command } = await import(file);
+	for (const command of commands) {
 		const data = command.data.toJSON();
 
 		command.privateGuildAndOwnerOnly ? ownerGuildCommands.push(data) : globalCommands.push(data);

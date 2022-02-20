357126596cfe050ad4e5892f60662946bab06153
CarelessInternet <myinternetisg00d@gmail.com>
2022-02-20 12:46:42 +0100

    fix(fetchCommandsData): use correct type for final json body

--- src/utils/fetchCommandsData.ts
+++ src/utils/fetchCommandsData.ts
@@ -1,27 +1,24 @@
 import fg from 'fast-glob';
-import type { RESTPatchAPIApplicationCommandJSONBody } from 'discord-api-types/v9';
+import type { RESTPostAPIApplicationCommandsJSONBody } from 'discord-api-types/v9';
 import { resolve } from 'path';
 import type { Command } from '../types';
 
 /**
  * Fetches the slash command builder data for each command and returns global commands array and owner commands array in an array
  */
-export const fetchCommandsData = async (): Promise<RESTPatchAPIApplicationCommandJSONBody[][]> => {
+export const fetchCommandsData = async () => {
 	const files = await fg(resolve(__dirname, '../commands/**/*.js'));
 
 	// add each command data into an array, owner only commands go into a seperate array
-	const ownerGuildCommands: RESTPatchAPIApplicationCommandJSONBody[] = [];
-	const globalCommands = await files.reduce(
-		async (acc: Promise<RESTPatchAPIApplicationCommandJSONBody[]>, file: string) => {
-			const accumulator = await acc;
-			const { default: command }: { default: Command } = await import(file);
-			const data = command.data.toJSON();
+	const ownerGuildCommands: RESTPostAPIApplicationCommandsJSONBody[] = [];
+	const globalCommands: RESTPostAPIApplicationCommandsJSONBody[] = [];
 
-			command.privateGuildAndOwnerOnly ? ownerGuildCommands.push(data) : accumulator.push(data);
-			return acc;
-		},
-		Promise.resolve([])
-	);
+	for await (const file of files) {
+		const { default: command }: { default: Command } = await import(file);
+		const data = command.data.toJSON();
+
+		command.privateGuildAndOwnerOnly ? ownerGuildCommands.push(data) : globalCommands.push(data);
+	}
 
 	return [globalCommands, ownerGuildCommands];
 };

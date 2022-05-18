09d69e1559277447b73ae78a6577275e62140178
CarelessInternet <59174259+CarelessInternet@users.noreply.github.com>
2022-05-18 18:54:15 +0200

    refactor: remove kick and ban commands, and improve code (#159)

--- src/commands/utility/purge-servers.ts
+++ src/commands/utility/purge-servers.ts
@@ -9,28 +9,42 @@ const command: Command = {
 	data: new SlashCommandBuilder()
 		.setName('purge-servers')
 		.setDescription('Purges servers where bots outnumber the amount of users')
+		.addNumberOption((option) =>
+			option
+				.setName('ratio')
+				.setDescription(
+					'The amount of bots per user, the recommended amount is 3. Lower is riskier.'
+				)
+				.setMinValue(1)
+		)
 		.addBooleanOption((option) =>
-			option.setName('fake').setDescription('Whether to imitate purging servers')
+			option
+				.setName('fake')
+				.setDescription('Whether to imitate/fake purging servers. Default is true')
+				.setRequired(false)
 		),
 	execute: async ({ client, interaction }) => {
 		try {
 			await interaction.deferReply({ ephemeral: true });
 
-			const fake = interaction.options.getBoolean('fake');
+			const ratio = interaction.options.getNumber('ratio') ?? 3;
+			const fake = interaction.options.getBoolean('fake') ?? true;
 
 			// array of arrays: first dimension = shard, second dimension = array of booleans (true = should leave the server)
 			const shardGuildBooleanArray = await client.shard!.broadcastEval(
-				(c, { ownerGuildId, fake }) =>
+				(c, { ownerGuildId, fake, ratio }) =>
 					c.guilds.cache.map((guild) => {
+						const ownerGuild = guild.id === ownerGuildId;
+
 						let botCount = 0;
 						let userCount = 0;
 
 						guild.members.cache.forEach((member) => (member.user.bot ? botCount++ : userCount++));
 
-						// if there are more than 3 bots per user, leave the guild
-						const shouldLeave = userCount / botCount < 1 / 3;
+						// if there are more than x bots per user, leave the guild
+						const shouldLeave = userCount / botCount < 1 / ratio;
 
-						if (guild.id !== ownerGuildId) {
+						if (!ownerGuild) {
 							if (shouldLeave) {
 								if (!fake) {
 									guild.leave();
@@ -38,9 +52,9 @@ const command: Command = {
 							}
 						}
 
-						return shouldLeave;
+						return !ownerGuild && shouldLeave;
 					}),
-				{ context: { ownerGuildId: process.env.DISCORD_GUILD_ID, fake } }
+				{ context: { ownerGuildId: process.env.DISCORD_GUILD_ID, fake, ratio } }
 			);
 
 			const shardsServersCount = shardGuildBooleanArray.map((shard) => {
@@ -66,7 +80,7 @@ const command: Command = {
 					name: interaction.user.tag,
 					iconURL: interaction.user.displayAvatarURL({ dynamic: true })
 				})
-				.setTitle('Purge Servers')
+				.setTitle(`Purge Servers (${fake ? 'faked' : 'real'})`)
 				.addField('Purged Servers', purgedServers.toLocaleString(), true)
 				.addField('Servers Left', leftServers.toLocaleString(), true)
 				.addField('Amount of Servers Before', (purgedServers + leftServers).toLocaleString(), true)

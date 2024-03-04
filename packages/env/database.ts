49a65cf898dcb92255828df1ccbe684db36175dc
CarelessInternet <myinternetisg00d@gmail.com>
2024-03-04 03:23:42 +0100

    feat: different networks and volumes for dev and prod

--- packages/env/database.ts
+++ packages/env/database.ts
@@ -5,7 +5,7 @@ import { z } from 'zod';
 export const environment = createEnv({
 	server: {
 		DB_HOST: z.string().default('database'),
-		DB_PORT: z.coerce.number().default(3306),
+		DB_PORT: z.coerce.number().default(3307),
 		DB_DATABASE: z.string().default('ticketer'),
 		DB_USER: z.string().default('dev_container'),
 		DB_PASSWORD: z.string().default('12345678'),

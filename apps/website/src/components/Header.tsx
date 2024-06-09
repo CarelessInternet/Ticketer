572404f641ba8b8ad30523f5bbb9799966fcb98d
CarelessInternet <59174259+CarelessInternet@users.noreply.github.com>
2024-06-09 19:34:18 +0200

    fix(configuration-ticket-threads): make modal fields correctly optional (#385)

--- apps/website/src/components/Header.tsx
+++ apps/website/src/components/Header.tsx
@@ -1,5 +1,5 @@
 import type { PropsWithChildren } from 'react';
 
 export default function Header({ children }: PropsWithChildren) {
-	return <h3 className="text-lg font-bold sm:text-xl">{children}</h3>;
+	return <h3 className="mt-8 text-lg font-bold sm:text-xl">{children}</h3>;
 }

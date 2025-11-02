5ac6e3a1bc3c111cb32ec07ff1716aaf9d9445af
CarelessInternet <myinternetisg00d@gmail.com>
2025-11-02 18:36:32 +0000

    fix(env): transform umami to undefined if not set

--- packages/env/website.ts
+++ packages/env/website.ts
@@ -4,8 +4,8 @@ import z from 'zod';
 export const environment = createEnv({
 	client: {
 		NEXT_PUBLIC_SITE_URL: z.url(),
-		NEXT_PUBLIC_UMAMI_ID: z.string().nullish(),
-		NEXT_PUBLIC_UMAMI_URL: z.url().nullish(),
+		NEXT_PUBLIC_UMAMI_ID: z.preprocess((value) => value || undefined, z.string().nullish()),
+		NEXT_PUBLIC_UMAMI_URL: z.preprocess((value) => value || undefined, z.url().nullish()),
 	},
 	experimental__runtimeEnv: {
 		NEXT_PUBLIC_SITE_URL: process.env.NEXT_PUBLIC_SITE_URL,

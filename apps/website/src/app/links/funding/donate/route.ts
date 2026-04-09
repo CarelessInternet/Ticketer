49e009499906a20f7c37b4f13f3341f6c62addb9
Pyce <59174259+CarelessInternet@users.noreply.github.com>
2026-04-09 16:05:56 +0200

    chore(release): 3.6.0 (#442)

--- apps/website/src/app/links/funding/donate/route.ts
+++ apps/website/src/app/links/funding/donate/route.ts
@@ -5,7 +5,7 @@ import { analyticsTrack } from '@/lib/analytics';
 export async function GET(request: Request) {
 	analyticsTrack({
 		referrer: request.referrer,
-		title: 'Link - GitHub',
+		title: 'Link - Donate',
 		url: request.url,
 		userAgent: userAgent({ headers: await headers() }).ua,
 	});
